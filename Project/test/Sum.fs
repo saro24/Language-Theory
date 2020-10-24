97b6c654516057238a487dec4b00ce45757a641f
Sara Bouglam <35578160+saro24@users.noreply.github.com>
2020-10-24 10:33:21 +0200

    Update and rename Factorial3.fs to Sum.fs

---
+++ Project/test/Sum.fs
@@ -0,0 +1,15 @@
+BEGINPROG Sum
+
+/* This is a sample program that counts the sum of numbers from 0 to an input inserted by the user */
+
+  READ(number)              // Read a number from user input
+  sum:=0 
+WHILE (number >= 0) THEN
+   IF (number > 0) THEN
+       sum := sum + 1
+       number:= number - 1
+   ELSE 
+      PRINT(sum)   
+   ENDWHILE    
+ENDIF
+ENDPROG
